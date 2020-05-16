85d0e16ad979a149e6e85d576d1fed79e29dd7c0
Emma Allegrucci <emmallegrucci@gmail.com>
2020-05-15 22:25:52 -0700

    just need to fix setting home location on phone

--- app/src/main/java/com/example/assignment4/MapsActivity.java
+++ app/src/main/java/com/example/assignment4/MapsActivity.java
@@ -13,8 +13,11 @@ import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,6 +28,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -41,8 +45,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private boolean mLocationPermissionsGranted = false;
 
+    private Marker mHome;
+
     //widgets
     private EditText mHomeText;
+    private ImageView mUpdateLoc;
 
     // vars
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
@@ -55,6 +62,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         mHomeText = (EditText) findViewById(R.id.homeSearch);
+        mUpdateLoc = (ImageView) findViewById(R.id.ic_updateLoc);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -70,16 +78,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 if(actionId == EditorInfo.IME_ACTION_SEARCH ||
                 actionId == EditorInfo.IME_ACTION_DONE ||
                 event.getAction() == KeyEvent.ACTION_DOWN ||
-                event.getAction() == KeyEvent.KEYCODE_ENTER) {
+                event.getAction() == KeyEvent.KEYCODE_ENTER ||
+                event.getAction() == KeyEvent.KEYCODE_BUTTON_SELECT ||
+                event.getAction() == KeyEvent.KEYCODE_SEARCH) {
                     homeLocate();
                 }
                 return false;
             }
         });
+
+        mUpdateLoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: clicked refresh icon.");
+                getDeviceLocation();
+            }
+        });
+
+        hideSoftKeyboard();
     }
 
     private void homeLocate() {
         String homeStr = mHomeText.getText().toString();
+        Log.d(TAG, "homeLocate: homeStr: " + homeStr);
 
         Geocoder g = new Geocoder(MapsActivity.this);
         List<Address> list = new ArrayList<>();
@@ -97,37 +118,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        getLocationPermission();
 
         // Add a marker to SCU and move the camera
         LatLng home = new LatLng(37.35, -121.94);
         moveCamera(home, DEFAULT_ZOOM);
-        /*if (mLocationPermissionsGranted) {
-            //TODO: create update button that updates the marker
-            getDeviceLocation();
-
+        if (mLocationPermissionsGranted) {
+            //getDeviceLocation();
             mMap.setMyLocationEnabled(true);
             init();
             mMap.getUiSettings().setMyLocationButtonEnabled(true);
-        }*/
+        }
     }
 
     private void getDeviceLocation() {
+        Log.d(TAG, "getDeviceLocation");
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
         try {
             if(mLocationPermissionsGranted) {
+                Log.d(TAG, "getDeviceLocation: mLocationPermissionsGranted is true");
                 Task location = mFusedLocationProviderClient.getLastLocation();
                 location.addOnCompleteListener(new OnCompleteListener() {
                     @Override
@@ -135,8 +148,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         if (task.isSuccessful()) {
                             Log.d(TAG, "OnComplete: location found");
                             Location currentLocation = (Location) task.getResult();
-                            moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()),
-                                    DEFAULT_ZOOM);
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()),
+                                    DEFAULT_ZOOM));
                         } else {
                             Log.d(TAG, "OnComplete: current location is null");
                             Toast.makeText(MapsActivity.this, "unable to get current location", Toast.LENGTH_SHORT).show();
@@ -152,7 +165,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private void moveCamera(LatLng latLng, float zoom) {
         Log.d(TAG, "Moving camera to lat: " + latLng.latitude + " lng: " + latLng.longitude);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
-        mMap.addMarker(new MarkerOptions().position(latLng).title("Home"));
+        if (mHome != null) {
+            mHome.remove();
+        }
+        mHome = mMap.addMarker(new MarkerOptions().position(latLng).title("Home"));
+
+        hideSoftKeyboard();
     }
 
     private void getLocationPermission() {
@@ -173,12 +191,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-//    private void initMap() {
-//        SupportMapFragment mapFrag = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-//
-//        mapFrag.getMapAsync(MapsActivity.this);
-//    }
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         mLocationPermissionsGranted = false;
@@ -193,10 +205,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         }
                     }
                     mLocationPermissionsGranted = true;
-                    //initialize map
                 }
             }
 
         }
+    }
+
+    private void hideSoftKeyboard() {
+        Log.d(TAG, "hideSoftKeyboard");
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 }
